7c7034d46d20848568dea505c58ff51974f5229d
mohdkaifkhan123 <khank11111999@gmail.com>
2024-12-06 15:29:03 +0530

    Auth_Flow

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,10 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import sidebarReducer from './slices/sidebarSlice';
+import { combineReducers } from "@reduxjs/toolkit";
+import sidebarReducer from "./slices/sidebarSlice";
+import authReducer from "./slices/authSlice"; 
 
 const rootReducer = combineReducers({
   sidebar: sidebarReducer,
+  auth: authReducer, 
 });
 
 export default rootReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
-import rootReducer from './rootReducer';
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import rootReducer from "./rootReducer";
 
 // Persist configuration
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
-  // You can specify which slices to blacklist or whitelist
-  // blacklist: ['auth'], // This will prevent 'auth' slice from persisting
-  // whitelist: ['auth'], // You can also whitelist specific slices if you want
+  blacklist: ["auth"], // Exclude auth slice from persisting
 };
 
 // Create a persisted reducer
@@ -18,12 +16,11 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 // Configure the store
 const store = configureStore({
   reducer: persistedReducer,
-  // If you have non-serializable data, you can disable the serializability check
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: ['persist/PERSIST'], // You can add more actions to ignore here
-        ignoredPaths: ['someReducer.someNonSerializableValue'], // Ignore specific paths if needed
+        ignoredActions: ["persist/PERSIST"], // Ignore redux-persist actions
+        ignoredPaths: ['someReducer.someNonSerializableValue']
       },
     }),
 });
@@ -31,6 +28,5 @@ const store = configureStore({
 // Create a persistor
 const persistor = persistStore(store);
 
-// Export the store and persistor
 export { persistor };
 export default store;
