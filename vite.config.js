076f7f5de4f0750789a60836aef78864ff7bf580
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2024-12-19 16:32:00 +0530

    FIX: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Set the port for dev server (npm run dev)
+  },
+  preview: {
+    port: 3000, // Set the port for preview server (npm run preview)
+  },
 })
