a5161dbfcce634d143f9ed7c6c1db13731f578dc
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2025-01-07 15:58:46 +0530

    FIX: Notification count

--- src/components/common/Notifications.jsx
+++ src/components/common/Notifications.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import bellIcon from "../../assets/header/bell.svg"
-
+import React from "react";
+import bellIcon from "../../assets/header/bell.svg";
 
 const Notifications = () => {
-    return (
-        <div className="relative inline-block">
-            <img src={bellIcon} alt="Notification Bell" className="w-8 h-8" />
-            <div className="absolute -top-1 -right-1 bg-red-500 text-white text-xs font-bold w-5 h-5 rounded-full flex items-center justify-center">
-                6 {/* Replace with dynamic notification count */}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="relative inline-block">
+      <img src={bellIcon} alt="Notification Bell" className="w-8 h-8" />
+      <div className="absolute -top-1 -right-1 bg-red-500 text-white text-xs font-bold w-5 h-5 rounded-full flex items-center justify-center">
+        0 {/* Replace with dynamic notification count */}
+      </div>
+    </div>
+  );
+};
 
-export default Notifications
+export default Notifications;
